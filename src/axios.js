d0766529b9ed765e31ad8de74cff7aeec677466e
Cyrus <c_camanatad@unilab.com.ph>
2025-10-31 14:36:45 +0800

    update netlify and axios

--- src/axios.js
+++ src/axios.js
@@ -5,11 +5,13 @@ import axios from "axios";
 const isProduction = import.meta.env.PROD;
 
 axios.defaults.baseURL = isProduction
-  ? "/" // Netlify will proxy this to your EC2
+  ? "" // Netlify will proxy this to your EC2
   : "http://localhost:8000";
 
 axios.defaults.withCredentials = true;
 axios.defaults.withXSRFToken = true;
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+axios.defaults.headers.common['Accept'] = 'application/json';
 
 if (import.meta.env.DEV) {
   console.log("API Base URL:", axios.defaults.baseURL);
