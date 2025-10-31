5e2b88d08159e3b13dc760bc411687584796e1ed
Cyrus <c_camanatad@unilab.com.ph>
2025-10-31 12:35:04 +0800

    added prod api enpoint

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,24 +1,28 @@
-import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue(), tailwindcss()],
-  server: {
-    port: 3001,
-    proxy: {
-      '/api' : {
-        target: 'http://localhost:8000', // ✅ Use IPv4 explicitly
-        changeOrigin: true,
-        // rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    }
-  }
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [vue(), tailwindcss()],
+    server: {
+      port: 3001,
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL || "http://localhost:8000", // ✅ Use IPv4 explicitly
+          changeOrigin: true,
+          // rewrite: (path) => path.replace(/^\/api/, '')
+        },
+      },
+    },
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+  };
+});
